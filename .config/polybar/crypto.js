d1bde0dcff3b52d1060e01075361616e2f4e9abe
Tregan <tregan@tregan.me>
2021-05-02 04:01:12 -0600

    fix(crypto): log errors

--- .config/polybar/crypto.js
+++ .config/polybar/crypto.js
@@ -1,41 +1,63 @@
 #!/usr/bin/env node
-const WS = require("ws");
-
-const ws = new WS("wss://ws-feed.pro.coinbase.com");
+const WS = require('ws');
+const fs = require('fs');
 
 const subscriptions = [
   {
-    product: "ETH-USDC",
-    display: "ﲹ",
-    price: "0.00",
+    product: 'ETH-USDC',
+    display: 'ﲹ',
+    price: '0.00',
   },
   {
-    product: "BTC-USDC",
-    display: "ﴑ",
-    price: "0.00",
+    product: 'BTC-USDC',
+    display: 'ﴑ',
+    price: '0.00',
   },
 ];
 
-ws.on("message", (data) => {
-  const body = JSON.parse(data.toString());
+let ws;
 
-  // find subscription
-  const subscription = subscriptions.find((s) => s.product === body.product_id);
-  if (!subscription) return;
+function open() {
+  ws = new WS('wss://ws-feed.pro.coinbase.com');
 
-  const price = parseFloat(body.price).toFixed(2);
-  subscription.price = price;
+  ws.on('message', (data) => {
+    const body = JSON.parse(data.toString());
 
-  const output = subscriptions.map((s) => `${s.display} ${s.price}`).join("  ");
-  console.log(output);
-});
+    // find subscription
+    const subscription = subscriptions.find(
+      (s) => s.product === body.product_id
+    );
+    if (!subscription) return;
 
-ws.on("open", () => {
-  ws.send(
-    JSON.stringify({
-      type: "subscribe",
-      product_ids: ["ETH-USDC", "BTC-USDC"],
-      channels: ["ticker"],
-    })
-  );
+    const price = parseFloat(body.price).toFixed(2);
+    subscription.price = price;
+
+    const output = subscriptions
+      .map((s) => `${s.display} ${s.price}`)
+      .join('  ');
+    console.log(output);
+  });
+
+  ws.on('open', () => {
+    ws.send(
+      JSON.stringify({
+        type: 'subscribe',
+        product_ids: ['ETH-USDC', 'BTC-USDC'],
+        channels: ['ticker'],
+      })
+    );
+  });
+
+  ws.on('error', (err) => {
+    console.log(subscriptions.map((a) => a.display).join(' ') + ' ');
+    fs.appendFile(('./crypto.log', '----------\n' + err + '\n\n\n'));
+  });
+}
+
+open();
+
+process.on('SIGINT', () => {
+  console.log('\nClosing crypto');
+  ws.close();
+  process.exit();
 });
